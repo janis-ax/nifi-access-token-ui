274ea2c43550b6e3777f553c3ccc1667308ae418
janis-ax <janis.ax@icloud.com>
2021-08-01 23:00:16 +0200

    :lipstick:
    add layout

--- src/server.py
+++ src/server.py
@@ -1,4 +1,6 @@
 from flask import render_template, request, jsonify, make_response, Flask
+# import nipyapi
+import dotenv
 
 app = Flask(__name__)
 
@@ -17,4 +19,13 @@ def ping():
 
     return res
 
-app.run(debug=True, port=5005)
+@app.route("/token", methods=["POST"])
+def token():
+    pass
+    # nipyapi.security.set_service_ssl_context
+    # nipyapi.security.service_login()
+
+
+if __name__ == "__main__":
+    ca_path = "/Users/janisax/git/nifi-environment/certs"
+    app.run(debug=True, port=5005)
